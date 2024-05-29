578fa2b089f67ed63ff8512a5375931210c66c25
Oleh-Bedryk <bedrikleo@gmail.com>
2024-05-29 16:25:11 -0700

    Bobby Path Initial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BobbyPath.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+@Autonomous (group = "drive")
+public class BobbyPath extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        SampleMecanumDrive drive = new Sampl eMecanumDrive(hardwareMap);
+
+        drive.setPoseEstimate(new Pose2d(50, 50, Math.toRadians(270)));
+        Trajectory cool = drive.trajectoryBuilder(new Pose2d(50, 50, 270))
+                        .lineTo(new Vector2d(50, -50)).build()
+
+        Trajectory swag == drive.trajectoryBuilder(new Pose2d(50, -50, Math.toRadians(180)))
+                                 .lineToLinearHeading(new Pose2d(-30, -50, Math.toRadians(90)))
+                                                      .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        drive.followTrajectory(cool);
+        drive.turn(Math.toRadians(-90));
+        drive.followTrajectory(swag);
+    }
+}
