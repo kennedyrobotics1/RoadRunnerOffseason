0f53eb5e61e53ff890399d90959fb6b1db45ba94
Oleh-Bedryk <bedrikleo@gmail.com>
2024-05-29 17:05:27 -0700

    Changed a few syntax errors and reformatted the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/FinalPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/FinalPath.java
@@ -7,19 +7,21 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
 @Autonomous (group = "drive")
-public class BobbyPath extends LinearOpMode {
+public class FinalPath extends LinearOpMode {
     @Override
     public void runOpMode() {
-        SampleMecanumDrive drive = new Sampl eMecanumDrive(hardwareMap);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         drive.setPoseEstimate(new Pose2d(50, 50, Math.toRadians(270)));
-        Trajectory cool = drive.trajectoryBuilder(new Pose2d(50, 50, 270))
-                        .lineTo(new Vector2d(50, -50)).build()
+        Trajectory cool = drive.trajectoryBuilder(new Pose2d(50, 50, Math.toRadians(270)))
+                        .lineTo(new Vector2d(50, -50))
+                        .build();
 
-        Trajectory swag == drive.trajectoryBuilder(new Pose2d(50, -50, Math.toRadians(180)))
-                                 .lineToLinearHeading(new Pose2d(-30, -50, Math.toRadians(90)))
-                                                      .build();
+        Trajectory swag = drive.trajectoryBuilder(new Pose2d(50, -50, Math.toRadians(180)))
+                        .lineToLinearHeading(new Pose2d(-30, -50, Math.toRadians(90)))
+                        .build();
 
         waitForStart();
 
