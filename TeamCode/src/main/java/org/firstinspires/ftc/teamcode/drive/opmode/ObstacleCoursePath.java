2afb4d9a9652f34f81fd405610fe89faa6a4bc20
isabellammjfk <izzysoccer14@icloud.com>
2024-09-03 17:32:03 -0700

    changed file name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ObstacleCoursePath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ObstacleCoursePath.java
@@ -5,24 +5,25 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Autonomous (group = "drive")
-public class izzyPath extends LinearOpMode {
+public class ObstacleCoursePath extends LinearOpMode {
     @Override
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         drive.setPoseEstimate(new Pose2d(-5, -30, Math.toRadians(270)));
 
-        Trajectory forwardToWall= drive.trajectoryBuilder(new Pose2d(-5, -30, Math.toRadians(270)))
+        Trajectory forwardToWall = drive.trajectoryBuilder(new Pose2d(-5, -30, Math.toRadians(270)))
                 .lineTo(new Vector2d(-5, -57))
                 .build();
 
-        Trajectory forwardToMosaic= drive.trajectoryBuilder(new Pose2d(-5, -57, Math.toRadians(90)))
+        Trajectory forwardToMosaic = drive.trajectoryBuilder(new Pose2d(-5, -57, Math.toRadians(90)))
                 .lineTo(new Vector2d(40, -57))
                 .build();
-        Trajectory yeetTheRobotToPark= drive.trajectoryBuilder(new Pose2d(40, -57, Math.toRadians(90)))
+        Trajectory yeetTheRobotToPark = drive.trajectoryBuilder(new Pose2d(40, -57, Math.toRadians(90)))
                 .lineTo(new Vector2d(40, -35))
                 .build();
 
